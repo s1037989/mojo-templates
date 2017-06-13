6dc9591d579b008ea107ca2656d92dfc182d3fc1
Stefan Adams <sadams@Stefans-iMac.local>
2017-06-13 16:26:22 -0500

    first commit

---
+++ myapp.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+
+get '/' => 'index';
+get '/internal';
+get '/color/:color' => {color => ''} => sub {
+  my $c = shift;
+  $c->param('color') ? $c->session(color => $c->param('color')) : delete $c->session->{color};
+  $c->redirect_to($c->req->headers->referrer);
+} => 'color';
+
+app->start;
